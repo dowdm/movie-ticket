89b552b927f8b3610793c12c74aa76db8e263001
Matt Dowdney and Matt Miller <matt@>
2018-04-03 10:30:58 -0700

    add jquery logic and calculate prototype method

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,51 @@
 //Business Logic
 
-function Movie(title, times, rating, maxPrice) {
+function Movie(title, times, rating) {
   this.title = title;
   this.times = times;
   this.rating = rating;
-  this.maxPrice = maxPrice;
+}
+var Animated = new Movie ("Animated", ["2:00PM", "4:00PM", "7:00PM", "9:00PM"], 2);
+var romCom = new Movie ("Rom-Com", ["2:00PM", "4:00PM", "7:00PM", "9:00PM"], 3);
+var cgiAction = new Movie ("CGI-Action", ["2:00PM", "4:00PM", "7:00PM", "9:00PM"], 4);
+var crappyHorror = new Movie ("Crappy-Horror", ["2:00PM", "4:00PM", "7:00PM", "9:00PM"], 4);
+
+function discount(age, time){
+  if (age === "1" || age === "4"){
+    return -3;
+  } else if (time === "1" || time === "2"){
+    return -2;
+  } else {
+    return 0;
+  }
 }
 
+function Order(title, time, maxPrice, quantity, discount) {
+  this.title = title;
+  this.time = time;
+  this.maxPrice = maxPrice;
+  this.quantity = quantity;
+  this.discount = discount;
+}
+
+Order.prototype.calculate = function() {
+  return (this.maxPrice + this.discount) * this.quantity;
+}
 
 //User Logic
 $(function() {
   $("#form").submit(function(event) {
+
+    var inputAge = $("#age").val();
+    var inputTitle = $("#movie-titles").val();
+    var inputTime = $("#movie-times").val();
+    var inputQuantity = $("#quantity").val();
+    var userDiscount = discount(inputAge, inputTime);
+    var userPrice = 10;
+    var newOrder = new Order(inputTitle, inputTime, userPrice, inputQuantity, userDiscount);
+
+    console.log(newOrder.calculate());
+    console.log(userDiscount);
 
     event.preventDefault();
   });
